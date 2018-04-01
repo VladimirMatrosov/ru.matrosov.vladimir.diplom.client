a4e808b5c91baab314be564c2b6b08b9adada901
Valeriy <lonelydeveloper97@gmail.com>
2018-04-01 15:12:09 +0300

    RecyclerView example

--- app/src/main/java/ru/matrosov/vladimir/diplom/client/UsersFragment.java
+++ app/src/main/java/ru/matrosov/vladimir/diplom/client/UsersFragment.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,36 +46,66 @@ public class UsersFragment extends Fragment {
     @SuppressLint("ResourceType")
     void onResponse(ServerResponse response) {
         if (response.getStatus() == 0) {
-            List<User> users = (ArrayList) response.getResponseObj();
-            ScrollView scrollView = new ScrollView(this.getContext());
-            RelativeLayout relativeLayout = new RelativeLayout(this.getContext());
 
-            TextView textView = new TextView(this.getContext());
-            textView.setText("Имя: , Фамилия: , Почта:, Должность: ");
-            textView.setId(13);
+            ArrayList<User> users = new ArrayList<>();
+            User user = new User();
+            user.setEmail("1");
+            User user2 = new User();
+            user2.setEmail("2");
+            User user3 = new User();
+            user3.setEmail("3");
+            User user4  = new User();
+            user4.setEmail("4");
 
-            RelativeLayout.LayoutParams textViewParams = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT);
-            textViewParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-            relativeLayout.addView(textView, textViewParams);
 
-            ImageButton imageButton = new ImageButton(this.getContext());
-            imageButton.setImageResource(R.drawable.ic_menu_chat);
+            users.add(user);
+            users.add(user2);
 
-            RelativeLayout.LayoutParams imageButParam = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT);
-            imageButParam.addRule(RelativeLayout.ALIGN_TOP, textView.getId());
-            imageButParam.addRule(RelativeLayout.RIGHT_OF, textView.getId());
-            relativeLayout.addView(imageButton, imageButParam);
+            users.add(user3);
 
-            scrollView.addView(relativeLayout);
-            ((FrameLayout)getView()).addView(scrollView);
+            users.add(user4);
 
-            Toast.makeText(this.getContext(), "Response:" + response.toString(), Toast.LENGTH_LONG).show();
+
+            RecyclerView recyclerView = getView().findViewById(R.id.recyclerView);
+
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+
+            RecyclerView.Adapter<MyUserViewHolder> userViewHolderAdapter = new RecyclerView.Adapter<MyUserViewHolder>() {
+                @NonNull
+                @Override
+                public MyUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                    View view = LayoutInflater.from(getContext()).inflate(R.layout.user_cell_layout, parent, false);
+                    return new MyUserViewHolder(view);
+                }
+
+                @Override
+                public void onBindViewHolder(@NonNull MyUserViewHolder holder, int position) {
+                    holder.setName(users.get(position).getEmail());
+                }
+
+                @Override
+                public int getItemCount() {
+                    return users.size();
+                }
+            };
+            recyclerView.setAdapter(userViewHolderAdapter);
+
+            userViewHolderAdapter.notifyDataSetChanged();
         } else {
             Toast.makeText(this.getContext(), "В системе нет пользователей", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    class MyUserViewHolder extends RecyclerView.ViewHolder {
+        private TextView userTextView;
+
+        public MyUserViewHolder(View itemView) {
+            super(itemView);
+            this.userTextView = itemView.findViewById(R.id.username);
+        }
+
+        void setName(String text) {
+            userTextView.setText(text);
         }
     }
 }
